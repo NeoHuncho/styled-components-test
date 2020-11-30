53706e1440f1b229c0305c62d6e983d3c6a15d7d
NeoHuncho <william.guinaudie@gmail.com>
2020-11-30 23:01:27 +0100

    added many different functionality of styled-components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import React from 'react'
 import styled from 'styled-components';
+import { Link } from 'react-router-dom'; //demonstration purposes
 
-const MainWrapper= styled.section`
+import Button from './components/Button'
+const MainWrapper = styled.section`
 width: 100%;
 max-width: 1200px;
 margin: 0 auto;
+display: flex;
+flex-direction: column;
 `
+const StyledLink = styled(Link)`//demonstration purposes
+`;
+
+const PaginationWrapper = styled.div`
+display:flex;
+width:100%;
+justify-content:${({page})=>{
+    if(page=='first') return 'flex-end';
+    else if (page== 'middle') return 'space-between';
+    else return 'flex-start';
+}}`
 const App = () => {
+    //NEVER PUT STYYLED COMPONENT INSIDE HERE
     return (
         <MainWrapper>
-        
+            <Button primary margin='5rem'>My Primary button </Button>
+            <Button >My Button</Button>
+            <PaginationWrapper page='first'/**page='middle'*//**page='' */>
+                <Button>Page 1</Button>
+            </PaginationWrapper>
         </MainWrapper>
     )
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,54 @@
 import React from 'react'
 
-import styled from 'styled-components';
-const Button = () => {
-    return (
-        <div>
-          <button>Push me! </button>
-        </div>
-    )
-}
+import styled, { css } from 'styled-components';
 
+import { Wrapper,FadeIn} from './styled/index';
+
+const OutterWrapper = styled.div`
+width:100%;
+
+`
+
+const StyledButton = styled.button`
+  background-color:white;
+  color: palevioletred;
+  font-size: 1.2rem;
+  margin: ${({ margin }) => margin || '2rem'};
+  padding: .5rem 1rem;
+  border: 1px solid palevioletred;
+  border-radius : 3px;
+  animation:2s ${FadeIn} ease-in;
+
+  ${({ primary }) => primary && css`
+    background-color:palevioletred;
+    color:white;
+    box-shadow:0 .5rem 1.5rem rgba(0,0,0,0.2)
+    `}
+
+  &:hover{
+    color:white;
+    background-color:green;
+  }
+  .someClass{
+    color:blue;
+    font-size:10px;
+  }
+`;
+
+const SuperButton = styled(StyledButton)`
+font-size:2.5rem
+`;
+
+const Button = ({ primary, children, margin }) => {
+  return (
+     <Wrapper > 
+    <StyledButton primary={primary} margin={margin}>
+      {children}
+        <p className='someClass'>Test</p>
+    </StyledButton>
+
+     </Wrapper>/*imported from styled folder*/ 
+  )
+}
+//    <SuperButton primary={primary}>{children} </SuperButton> you can try it ouf by putting in the Button component
 export default Button;
